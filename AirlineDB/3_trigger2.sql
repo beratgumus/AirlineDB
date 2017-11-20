3f73a6bab9ba4d6d7b689e29bf80e27f3a206fad
Bekir Uzun <bekiruzun8000@gmail.com>
2017-11-21 00:59:01 +0300

    Added new trigger
    Added new trigger that controls availablity of  flight. It replaces 'INSERT' of  SEAT_RESERVATION table

---
+++ AirlineDB/3_trigger2.sql
@@ -0,0 +1,29 @@
+﻿-- yeni bilet alınmadan önce boş koltuk var mı kontrol edelim
+
+-- NOT: şu anda sadece tek INSERT destekleniyor (Aynı anda 2 bilet alınmayacağından makul gibi). 
+-- Toplu eklemeleri bu trigger'i oluşturmadan önce yapınız.
+USE Airline1
+GO
+CREATE TRIGGER TRG_control_availability
+        ON SEAT_RESERVATION
+        INSTEAD OF INSERT
+AS
+BEGIN
+	
+	DECLARE @available int
+	SET @available = 
+	(
+		SELECT Number_of_available_seats 
+		FROM LEG_INSTANCE AS LI, inserted
+		WHERE LI.Flight_no = inserted.Flight_no
+		AND LI.Leg_no = inserted.Leg_no
+		AND LI.[Date] = inserted.[Date]
+	)
+
+	IF( @available > 0 )
+		INSERT INTO SEAT_RESERVATION
+		SELECT * FROM inserted
+	ELSE
+		PRINT 'There is no available seats for that leg instance'
+
+END
