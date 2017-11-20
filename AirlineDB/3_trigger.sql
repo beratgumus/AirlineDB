e02775d684ef4292392b443164399ae392b2bf25
Bekir Uzun <bekiruzun8000@gmail.com>
2017-11-20 23:38:38 +0300

    Fixed trigger

--- AirlineDB/3_trigger.sql
+++ AirlineDB/3_trigger.sql
@@ -1,4 +1,4 @@
--- yeni bilet alýndýðýnda satýn alýnabilen koltuk sayýsýný düþürelim
+ï»¿-- yeni bilet alÄ±ndÄ±ÄŸÄ±nda satÄ±n alÄ±nabilen koltuk sayÄ±sÄ±nÄ± dÃ¼ÅŸÃ¼relim
 USE Airline1
 GO
 CREATE TRIGGER TRG_decrease_available_seats
@@ -9,15 +9,17 @@ BEGIN
 
     UPDATE  LEG_INSTANCE
     SET     Number_of_available_seats = Number_of_available_seats -
-	(SELECT COUNT(*)
-	FROM inserted,LEG_INSTANCE LI
-	WHERE LI.Flight_no = inserted.Flight_no
-		and LI.Leg_no = inserted.Leg_no
-		and LI.[Date] = inserted.[Date])
+	(
+		SELECT COUNT(*)
+		FROM inserted
+		WHERE LEG_INSTANCE.Flight_no = inserted.Flight_no
+		AND LEG_INSTANCE.Leg_no = inserted.Leg_no
+		AND LEG_INSTANCE.[Date] = inserted.[Date]
+	)
 	WHERE LEG_INSTANCE.Flight_no IN  
       (SELECT Flight_no FROM inserted)
-	  and LEG_INSTANCE.Leg_no IN  
+	AND LEG_INSTANCE.Leg_no IN  
       (SELECT Leg_no FROM inserted)
-	  and LEG_INSTANCE.[Date] IN  
+	AND LEG_INSTANCE.[Date] IN  
       (SELECT [Date] FROM inserted)
 END
