c0adfa1f0c35b9059ccf936f7d1eeabae1222038
Bekir Uzun <bekiruzun8000@gmail.com>
2017-11-22 20:51:19 +0300

    Added trigger test and cursor example

---
+++ AirlineDB/tests/example_cursor.sql
@@ -0,0 +1,36 @@
+﻿-- cursor kullanımı için bir örnek.
+-- bir tablonun her satırı için işlem yapılacağında kullanılıyor. (döngü)
+-- Bu kod tüm airport code'larını listeler
+
+USE Airline1
+GO
+
+-- airport kodlarını tutacak geçici değişkenimiz
+DECLARE @airportcode nchar(3);
+
+-- satırları gezmek için kullanacağımız cursor'umuz
+DECLARE airport_cursor CURSOR 
+LOCAL STATIC READ_ONLY FORWARD_ONLY
+FOR 
+SELECT Airport_code FROM AIRPORT; 
+-- SELECT kısmı ile cursorun gösterdiği satırın hangi sütunlarını seçeceğimizi belirledik
+-- bkz. FETCH NEXT FROM
+
+-- cursoru aktifleştirelim
+OPEN airport_cursor;
+
+-- cursorun gösterdiği satırdaki veriyi alalım (ilk satır)
+FETCH NEXT FROM airport_cursor INTO @airportcode;
+
+-- döngümüzü başlatalım
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	-- az önce aldığımız veriyi yazdıralım
+	PRINT @airportcode;
+
+	-- bir sonraki veriyi alalım
+	FETCH NEXT FROM airport_cursor INTO @airportcode;
+END
+
+CLOSE airport_cursor;
+DEALLOCATE airport_cursor;
---
+++ AirlineDB/tests/trigger_airplane_test.sql
@@ -0,0 +1,12 @@
+﻿USE [Airline1]
+GO
+
+INSERT INTO [dbo].[AIRPLANE] ([Airplane_id],[Total_number_of_seats] ,[Airplane_type])
+VALUES
+(9901, 845, 'Airbus A380'),
+(9902, 5, 'Airbus A380'),
+(9903, 850, 'Airbus A380');
+
+GO
+
+
