b57a90feeddc0a0159e365326152139e3f7cdeb8
Bekir Uzun <bekiruzun8000@gmail.com>
2017-12-28 17:51:40 +0300

    Added a trigger and its tests

---
+++ AirlineDB2/3_trigger1.sql
@@ -0,0 +1,29 @@
+﻿USE Airline2
+GO
+
+DROP TRIGGER IF EXISTS [dbo].[TRG_control_company]
+GO
+
+CREATE TRIGGER TRG_control_company
+        ON FLIGHT
+        INSTEAD OF INSERT
+AS
+BEGIN
+
+	DECLARE @company_id int;
+	SELECT @company_id = Company_id FROM inserted;
+	
+	DECLARE @company_type nvarchar(10);
+
+	SELECT @company_type = C.Company_type
+		FROM COMPANY as C
+		WHERE C.Id = @company_id;
+
+	IF( @company_type = 'Airline' )
+		INSERT INTO FLIGHT
+		SELECT * FROM inserted;
+	ELSE
+		RAISERROR('You cant select an airplane company for flights. Select an airline company instead.', 16, 1)
+		--PRINT 'You cant select an airplane company for flights. Select an airline company instead.';
+
+END
---
+++ AirlineDB2/3_xtrigger_tests.sql
@@ -0,0 +1,16 @@
+﻿-- trigger'ların doğru çalışıp çalışmadığını test etmek için yazılmış olan query'ler
+
+USE Airline2
+GO
+
+--==♦ FLIGHT tablosundaki TRG_control_company hata testi ♦==--
+INSERT INTO FLIGHT (Flight_number, Company_id, Weekdays) 
+VALUES 
+(998, 1, '1,2,3,4,5,6,7'); -- company = Türk Hava Yolları, hata olmamalı
+
+DELETE FROM FLIGHT
+WHERE Flight_number = 998;
+
+INSERT INTO FLIGHT (Flight_number, Company_id, Weekdays) 
+VALUES 
+(999, 7, '1,2,3,4,5,6,7'); -- company = Airbus S.A.S, triggerdan hata gelmeli
