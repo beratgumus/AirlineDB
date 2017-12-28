86a5cb995bcd476999b9fb0a14bbe884e3ab98f0
Berat Gumus <beratgumus9@gmail.com>
2017-12-29 00:14:10 +0300

    Added new quey and some data to see better output

--- AirlineDB2/5_select.sql
+++ AirlineDB2/5_select.sql
@@ -1,6 +1,6 @@
 USE Airline2
 
--- Uyrugu Japonya olan m�steriler tarafindan en �ok tercih edilen havayolu sirketleri
+-- Uyrugu Japonya olan musteriler tarafindan en cok tercih edilen havayolu sirketleri
 
 SELECT	CO.Name,COUNT(*) as Musteri_sayisi
 FROM CUSTOMER as C,SEAT_RESERVATION as SR,FLIGHT as F,COMPANY as CO
@@ -10,4 +10,20 @@ AND		F.Company_id=CO.Id
 AND		C.Country='Japan'
 GROUP BY CO.Name
 ORDER BY COUNT(*) desc;
+
+
+--Son 2 ay icerisinde Tokyo`daki havaalanina inis yapan yolcularin uyruklara gore dagilimi --degisimi gormek icin ay farkini 2den 3e cikar
+
+SELECT	C.Country,COUNT(*) as Yolcu_sayisi
+FROM AIRPORT as A,LEG_INSTANCE as LI,CUSTOMER as C,SEAT_RESERVATION as SR
+WHERE	A.Airport_code=LI.Arrival_airport_code
+AND		LI.Flight_no=SR.Flight_no
+AND		LI.Leg_no=SR.Leg_no
+AND		LI.Date=SR.Date
+AND		SR.Customer_id=C.Id
+AND		A.City='Tokyo'
+AND		Li.Date > DATEADD(m, -2, getdate())
+GROUP BY C.Country
+ORDER BY COUNT(*) desc;
+
 
